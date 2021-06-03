549a75bbe41212801ebb8c8f2af61dbd3023961f
Fabio Bueno <fabio.boliveira@digisystem.com.br>
2021-06-03 14:25:08 -0300

    Reordenação e ajustes de css nos ícones no footer mobile

--- src/components/layout/footerMenuMobile/index.jsx
+++ src/components/layout/footerMenuMobile/index.jsx
@@ -18,14 +18,19 @@ const FooterMenuMobile = () => {
                     <Icon name='home' className='mr-0' />
                     <span>Home</span>
                   </div>
-                  <div className={currentPath.includes('/my-projects') && 'active'} onClick={() => history.push("/my-projects")}>
-                    {/* <i className="fas fa-guitar"></i> */}
-                    <Icon name='rocket' className='mr-0' />
-                    <span>Meus projetos</span>
-                  </div>
                   <div className={currentPath === '/search' && 'active'} onClick={() => history.push("/search")}>
                     <Icon name='search' className='mr-0' />
                     <span>Buscar</span>
+                  </div>
+                  <div className={currentPath === '/new' && 'active'} onClick={() => history.push("/new")}>
+                    {/* <i className="fas fa-plus"></i> */}
+                    <Icon name='plus' className='mr-0' />
+                    <span>Novo</span>
+                  </div>
+                  <div className={currentPath.includes('/my-projects') && 'active'} onClick={() => history.push("/my-projects")}>
+                    {/* <i className="fas fa-guitar"></i> */}
+                    <Icon name='rocket' className='mr-0' />
+                    <span>Projetos</span>
                   </div>
                   {/* <div className={currentPath === '/feed' && 'active'} onClick={() => history.push("/feed")}>
                     <Icon name='globe' className='mr-0' />
@@ -35,11 +40,6 @@ const FooterMenuMobile = () => {
                     {/* <i className="fas fa-bell"></i> */}
                     <Icon name='bell' className='mr-0' />
                     <span>Notificações</span>
-                  </div>
-                  <div className={currentPath === '/new' && 'active'} onClick={() => history.push("/new")}>
-                    {/* <i className="fas fa-plus"></i> */}
-                    <Icon name='plus' className='mr-0' />
-                    <span>Novo</span>
                   </div>
               </div>
             </footer>
